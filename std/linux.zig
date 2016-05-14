d9cfb61cb72041220522e62194da4e2e5ffe33a7
Travis McDemus <travis@mcdem.us>
2016-05-14 18:45:29 -0400

    Rename files to be consistent with arch

--- std/linux.zig
+++ std/linux.zig
@@ -1,7 +1,7 @@
 const arch = switch (@compile_var("arch")) {
     x86_64 => @import("linux_x86_64.zig"),
     i386 => @import("linux_i386.zig"),
-    armv7 => @import("linux_arm_v7.zig"),
+    armv7 => @import("linux_armv7.zig"),
     else => @compile_err("unsupported arch"),
 };
 const errno = @import("errno.zig");
